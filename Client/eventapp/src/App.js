79969a87d012cfdc6b6210b6ef14cd1ba9a6f852
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2025-02-06 15:10:31 -0500

    test

--- Client/eventapp/src/App.js
+++ Client/eventapp/src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+
+import events from './images/events.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">LinxUp</Typography>
+        <img src={events} alt="events" height="60" />
+        </AppBar>
+        <Grow in>
+            <Container>
+            <Grid container justifyContent="space-between" alignItems="stretch">
+                <Grid item xs={12} sm={7}> <Posts />
+                </Grid>
+                <Grid item xs={12} sm={7}> <Form />
+                </Grid>
+                <Grid item xs={12} sm={7}>
+                </Grid>
+                <Grid item>
+                    
+                </Grid>
+            </Grid>
+            </Container>
+            </Grow>
+      </Container>
+     
+    );
 }
 
 export default App;

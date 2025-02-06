c5ccf7b56185f0bb7ed9a285ef55e84ca930bf0b
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2025-02-06 15:47:32 -0500

    finished jsx structure

--- Client/eventapp/src/App.js
+++ Client/eventapp/src/App.js
@@ -1,33 +1,36 @@
-import React from "react";
+import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import events from './images/events.png';
 
 const App = () => {
-    return (
-      <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">LinxUp</Typography>
-        <img src={events} alt="events" height="60" />
-        </AppBar>
-        <Grow in>
-            <Container>
-            <Grid container justifyContent="space-between" alignItems="stretch">
-                <Grid item xs={12} sm={7}> <Posts />
-                </Grid>
-                <Grid item xs={12} sm={7}> <Form />
-                </Grid>
-                <Grid item xs={12} sm={7}>
-                </Grid>
-                <Grid item>
-                    
-                </Grid>
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Event</Typography>
+        <img className={classes.image} src={events} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
-            </Container>
-            </Grow>
-      </Container>
-     
-    );
-}
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
